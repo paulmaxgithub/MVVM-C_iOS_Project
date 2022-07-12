1b9487c1628d830a760c0fa9b2ddc54f8559a77c
paulmax <ipaulmax@icloud.com>
2022-07-13 00:31:17 +0300

    Initial Commit

---
+++ MVVM+C_iOS_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM+C_iOS_Project
+//
+//  Created by PaulmaX on 13.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
